111c2ee0bae9e84faa78261cde2fe926759e6ec4
Brian Burridge <bburridg@gmail.com>
2015-03-03 11:35:41 -0500

    Added pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.all.order('created_at desc')
+    @posts = Post.order('created_at desc').page(params[:page])
   end
 end
