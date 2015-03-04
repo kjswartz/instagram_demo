03f580184e3c9d27d3a332855e0d9c16572ea966
Brian Burridge <bburridg@gmail.com>
2015-03-04 10:08:36 -0500

    Added navbar with current user display and current_user functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :current_user
+
+  helper_method :current_user, :signed_in?
+
+  def current_user
+    @current_user ||= User.find_by(username: 'brian')
+  end
+
+  def signed_in?
+    return true if current_user.present?
+  end
 end
