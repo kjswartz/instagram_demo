a258519597a6af6e7b5804728e0bd3a19336acb1
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-03-08 14:08:38 -0400

    changed ENV variables.

--- config/initializers/carrierwave_fog.rb
+++ config/initializers/carrierwave_fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
-    :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY'],                        # required
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                        # required
     :endpoint               => 'http://s3.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = ENV['S3_BUCKET']                         # required
